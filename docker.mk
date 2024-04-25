5ad3d827af49ed90bae6184fc1307442fb7fa553
Adrien Poupard <1469248+apoupard@users.noreply.github.com>
2024-04-25 15:45:14 +0200

    chore(dev.yml): update make-file value to 'libs.mk' for consistency chore(dev.yml): update PKG_GITHUB_USERNAME value to use github.actor for clarity chore(dev.yml): update PKG_GITHUB_TOKEN value to use secrets.GITHUB_TOKEN for security chore(dev.yml): update DOCKER_PUBLISH_USERNAME value to use github.actor for consistency chore(dev.yml): update DOCKER_PROMOTE_USERNAME value to use github.DOCKER_IO_USERNAME for clarity chore(sec.yml): remove unused PKG_GITHUB_USERNAME and PKG_GITHUB_TOKEN secrets chore(sec.yml): add packages read permission for security-events chore(docker.mk): update IM_APP_NAME, IM_SCRIPT_NAME, KEYCLOAK_NAME, and KEYCLOAK_AUTH_NAME values to remove DOCKER_REPOSITORY prefix for simplicity chore(docker.mk): update docker-im-gateway-publish and docker-script-publish to tag images with ghcr.io/komune-io/ prefix for consistency chore(docker.mk): update docker-im-gateway-promote and docker-script-promote to push images to docker.io/komune/ for promotion chore(docker.mk): update docker-keycloak-publish and docker-keycloak-auth-publish to tag images with ghcr.io/komune-io/ prefix for consistency chore(docker.mk): update docker-keycloak-promote and docker-keycloak-auth-promote to push images to docker.io/komune/ for promotion
    The changes were made to improve consistency in naming conventions, enhance security by using appropriate secrets, and simplify the Docker image tagging and promotion process for better clarity and maintainability.

--- docker.mk
+++ docker.mk
@@ -3,60 +3,113 @@ DOCKER_REPOSITORY = ghcr.io/
 
 include ./gradle.properties
 
-IM_APP_NAME	   	 	:= ${DOCKER_REPOSITORY}komune-io/im-gateway
+IM_APP_NAME	   	 	:= im-gateway
 IM_APP_IMG	    	:= ${IM_APP_NAME}:${VERSION}
 IM_APP_PACKAGE	   	:= :im-api:api-gateway:bootBuildImage
 
-IM_SCRIPT_NAME	   	:= ${DOCKER_REPOSITORY}komune-io/im-script
+IM_SCRIPT_NAME	   	:= im-script
 IM_SCRIPT_IMG	    := ${IM_SCRIPT_NAME}:${VERSION}
 IM_SCRIPT_PACKAGE	:= :im-script:im-script-gateway:bootBuildImage
 
 KEYCLOAK_DOCKERFILE	:= infra/docker/keycloak/Dockerfile
 
-KEYCLOAK_NAME	    := ${DOCKER_REPOSITORY}komune-io/im-keycloak
+KEYCLOAK_NAME	    := im-keycloak
 KEYCLOAK_IMG        := ${KEYCLOAK_NAME}:${VERSION}
 
-KEYCLOAK_AUTH_NAME	:= ${DOCKER_REPOSITORY}komune-io/im-keycloak-auth
+KEYCLOAK_AUTH_NAME	:= im-keycloak-auth
 KEYCLOAK_AUTH_IMG   := ${KEYCLOAK_AUTH_NAME}:${VERSION}
 
 .PHONY: lint build test publish promote
 
 lint: docker-keycloak-lint
+
 build: docker-im-gateway-build docker-script-build docker-keycloak-build docker-keycloak-auth-build
+
 test:
 	echo 'No Tests'
-publish: docker-im-gateway-push docker-script-push docker-keycloak-push docker-keycloak-auth-push
-promote:
-	echo 'No Tests'
+
+publish: docker-im-gateway-publish docker-script-publish docker-keycloak-publish docker-keycloak-auth-publish
+
+promote: docker-im-gateway-promote docker-script-promote docker-keycloak-promote docker-keycloak-auth-promote
 
 ## im-gateway
 docker-im-gateway-build:
 	VERSION=${VERSION} ./gradlew build ${IM_APP_PACKAGE} --imageName ${IM_APP_IMG} -x test
 
-docker-im-gateway-push:
-	VERSION=${VERSION} docker push ${IM_APP_IMG}
+docker-im-gateway-publish:
+	@docker tag ${IM_APP_IMG} ghcr.io/komune-io/${IM_APP_IMG}
+	@docker push ghcr.io/komune-io/${IM_APP_IMG}
+
+docker-im-gateway-promote:
+	@docker tag ${IM_APP_IMG} ghcr.io/komune-io/${IM_APP_IMG}
+	@docker push docker.io/komune/${IM_APP_IMG}
 
 ## im-script
 docker-script-build:
 	VERSION=${VERSION} ./gradlew build ${IM_SCRIPT_PACKAGE} --imageName ${IM_SCRIPT_IMG} -x test
 
-docker-script-push:
-	@docker push ${IM_SCRIPT_IMG}
+docker-script-publish:
+	@docker tag ${IM_SCRIPT_IMG} ghcr.io/komune-io/${IM_SCRIPT_IMG}
+	@docker push ghcr.io/komune-io/${IM_SCRIPT_IMG}
+
+docker-script-promote:
+	@docker tag ${IM_SCRIPT_IMG} ghcr.io/komune-io/${IM_SCRIPT_IMG}
+	@docker push docker.io/komune/${IM_SCRIPT_IMG}
 
 ## Keycloak
 docker-keycloak-lint:
 	@docker run --rm -i hadolint/hadolint hadolint - < ${KEYCLOAK_DOCKERFILE}
 
 docker-keycloak-build:
+	@echo 'Build ${KEYCLOAK_IMG}'
 	./gradlew im-keycloak:keycloak-plugin:shadowJar
-	@docker build --no-cache --build-arg KC_HTTP_RELATIVE_PATH=/  --build-arg KEYCLOAK_VERSION=${KEYCLOAK_VERSION} -f ${KEYCLOAK_DOCKERFILE} -t ${KEYCLOAK_IMG} .
+	@docker buildx build \
+		--platform linux/arm64,linux/amd64 \
+		--no-cache --build-arg KC_HTTP_RELATIVE_PATH=/  \
+		--build-arg KEYCLOAK_VERSION=${KEYCLOAK_VERSION} \
+		-f ${KEYCLOAK_DOCKERFILE} \
+		-t ${KEYCLOAK_IMG} .
 
-docker-keycloak-push:
-	@docker push ${KEYCLOAK_IMG}
+docker-keycloak-publish:
+	@docker buildx build --push \
+		--platform linux/arm64,linux/amd64 \
+		--no-cache --build-arg KC_HTTP_RELATIVE_PATH=/ \
+		--build-arg KEYCLOAK_VERSION=${KEYCLOAK_VERSION} \
+		-f ${KEYCLOAK_DOCKERFILE} \
+		-t ghcr.io/komune-io/${KEYCLOAK_IMG} .
 
+docker-keycloak-promote:
+	@docker buildx build --push \
+		--platform linux/arm64,linux/amd64 \
+		--no-cache --build-arg KC_HTTP_RELATIVE_PATH=/ \
+		--build-arg KEYCLOAK_VERSION=${KEYCLOAK_VERSION} \
+		-f ${KEYCLOAK_DOCKERFILE} \
+		-t docker.io/komune/${KEYCLOAK_IMG} .
+
+
+# keycloak auth
 docker-keycloak-auth-build:
+	@echo 'Build ${KEYCLOAK_AUTH_IMG}'
 	./gradlew im-keycloak:keycloak-plugin:shadowJar
-	@docker build --no-cache --progress=plain --build-arg KC_HTTP_RELATIVE_PATH=/auth --build-arg KEYCLOAK_VERSION=${KEYCLOAK_VERSION} -f ${KEYCLOAK_DOCKERFILE} -t ${KEYCLOAK_AUTH_IMG} .
+	@docker buildx build \
+		--platform linux/arm64,linux/amd64 \
+		--no-cache --build-arg KC_HTTP_RELATIVE_PATH=/  \
+		--build-arg KEYCLOAK_VERSION=${KEYCLOAK_VERSION} \
+		-f ${KEYCLOAK_DOCKERFILE} \
+		-t ${KEYCLOAK_AUTH_IMG} .
 
-docker-keycloak-auth-push:
-	@docker push ${KEYCLOAK_AUTH_IMG}
+docker-keycloak-auth-publish:
+	@docker buildx build --push \
+		--platform linux/arm64,linux/amd64 \
+		--no-cache --build-arg KC_HTTP_RELATIVE_PATH=/ \
+		--build-arg KEYCLOAK_VERSION=${KEYCLOAK_VERSION} \
+		-f ${KEYCLOAK_DOCKERFILE} \
+		-t ghcr.io/komune-io/${KEYCLOAK_AUTH_IMG} .
+
+docker-keycloak-auth-promote:
+	@docker buildx build --push \
+		--platform linux/arm64,linux/amd64 \
+		--no-cache --build-arg KC_HTTP_RELATIVE_PATH=/ \
+		--build-arg KEYCLOAK_VERSION=${KEYCLOAK_VERSION} \
+		-f ${KEYCLOAK_DOCKERFILE} \
+		-t docker.io/komune/${KEYCLOAK_AUTH_IMG} .
