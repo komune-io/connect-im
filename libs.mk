ef3035f235b7e0735091811fa32a434a432521c4
Adrien <email = 1469248+apoupard@users.noreply.github.com>
2024-05-10 14:31:13 +0200

    fix(libs.mk): update publish target command to publishJsPackageToGithubRegistry instead of assembleJsPackage The publish target command has been updated to publishJsPackageToGithubRegistry instead of assembleJsPackage to accurately reflect the intended action of publishing the package to the GitHub registry.

--- libs.mk
+++ libs.mk
@@ -18,7 +18,7 @@ test:
 	./gradlew test
 
 publish:
-	VERSION=$(VERSION) PKG_MAVEN_REPO=github ./gradlew publish --info -x assembleJsPackage
+	VERSION=$(VERSION) PKG_MAVEN_REPO=github ./gradlew publish --info -x publishJsPackageToGithubRegistry
 
 promote:
 	VERSION=$(VERSION) PKG_MAVEN_REPO=sonatype_oss ./gradlew publish
