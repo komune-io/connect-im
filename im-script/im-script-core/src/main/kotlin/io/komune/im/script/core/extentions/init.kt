b260989e9423e26d8f0cbe745f0372fc81b6c896
Adrien <email = 1469248+apoupard@users.noreply.github.com>
2024-05-31 21:15:27 +0200

    refactor(init.kt): remove redundant log message in init function The commit removes a redundant log message in the init function to improve code readability and reduce unnecessary logging noise.

--- im-script/im-script-core/src/main/kotlin/io/komune/im/script/core/extentions/init.kt
+++ im-script/im-script-core/src/main/kotlin/io/komune/im/script/core/extentions/init.kt
@@ -3,7 +3,6 @@ package io.komune.im.script.core.extentions
 import org.slf4j.Logger
 
 suspend fun <T> init(logPrefix: String, logger: Logger, find: suspend () -> T?, create: suspend () -> T): T {
-    logger.info("$logPrefix...")
     val found = find()
     if(found != null) {
         logger.info("$logPrefix already exists")
