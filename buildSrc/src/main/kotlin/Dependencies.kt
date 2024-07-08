fa31c379ad0a9c65acbe8a82ba30473a0f9dde80
Adrien <email = 1469248+apoupard@users.noreply.github.com>
2024-07-08 10:50:41 +0200

    Update Connect version to 0.16.4

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -6,7 +6,7 @@ import io.komune.gradle.dependencies.add
 
 object Framework {
 	val fixers = FixersPluginVersions.fixers
-	val connect = "0.17.0-SNAPSHOT"
+	val connect = "0.16.4"
 }
 
 object PluginVersions {
