2823fbbece8350720469d37af381d8a5797e9ba8
Luc Heydel <luc@smartb.city>
2024-07-24 15:21:35 +0200

    fix serialization error in clients

--- im-f2/privilege/im-privilege-client/build.gradle.kts
+++ im-f2/privilege/im-privilege-client/build.gradle.kts
@@ -7,4 +7,5 @@ plugins {
 dependencies {
     commonMainApi(project(Modules.F2.privilegeDomain))
     Dependencies.Mpp.f2Client(::commonMainApi)
+    jvmMainImplementation("io.ktor:ktor-serialization-jackson:${Versions.ktor}")
 }
