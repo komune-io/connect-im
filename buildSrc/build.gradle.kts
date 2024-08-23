53d5cfa753cf1b0e15ae942f7a54b4a035644af7
Adrien <email = 1469248+apoupard@users.noreply.github.com>
2024-08-23 10:50:40 +0200

    chore(build.gradle.kts): update dependencies plugin version to 0.20.0-SNAPSHOT
    Update the dependencies plugin to the latest snapshot version to incorporate recent fixes and improvements. This ensures the build process benefits from the latest updates and enhancements provided by the plugin.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 dependencies {
-    implementation("io.komune.fixers.gradle:dependencies:0.19.0")
+    implementation("io.komune.fixers.gradle:dependencies:0.20.0-SNAPSHOT")
 }
 
 repositories {
