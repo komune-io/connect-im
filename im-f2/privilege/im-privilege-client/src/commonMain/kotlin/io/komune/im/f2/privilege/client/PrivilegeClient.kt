34f507d22847a9335c103982bede19da926daa30
Luc Heydel <luc@kosm.io>
2025-01-07 15:10:24 +0100

    add feature endpoints to privilege client

--- im-f2/privilege/im-privilege-client/src/commonMain/kotlin/io/komune/im/f2/privilege/client/PrivilegeClient.kt
+++ im-f2/privilege/im-privilege-client/src/commonMain/kotlin/io/komune/im/f2/privilege/client/PrivilegeClient.kt
@@ -7,8 +7,12 @@ import f2.client.ktor.F2ClientBuilder
 import f2.client.ktor.http.plugin.F2Auth
 import f2.dsl.fnc.F2SupplierSingle
 import f2.dsl.fnc.f2SupplierSingle
+import io.komune.im.f2.privilege.domain.FeatureApi
 import io.komune.im.f2.privilege.domain.PermissionApi
 import io.komune.im.f2.privilege.domain.RoleApi
+import io.komune.im.f2.privilege.domain.feature.command.FeatureDefineFunction
+import io.komune.im.f2.privilege.domain.feature.query.FeatureGetFunction
+import io.komune.im.f2.privilege.domain.feature.query.FeatureListFunction
 import io.komune.im.f2.privilege.domain.permission.command.PermissionDefineFunction
 import io.komune.im.f2.privilege.domain.permission.query.PermissionGetFunction
 import io.komune.im.f2.privilege.domain.permission.query.PermissionListFunction
@@ -40,7 +44,11 @@ fun privilegeClient(
 }
 
 @JsExport
-open class PrivilegeClient(private val client: F2Client): RoleApi, PermissionApi {
+open class PrivilegeClient(private val client: F2Client): FeatureApi, RoleApi, PermissionApi {
+    override fun featureDefine(): FeatureDefineFunction = client.function(this::featureDefine.name)
+    override fun featureGet(): FeatureGetFunction = client.function(this::featureGet.name)
+    override fun featureList(): FeatureListFunction = client.function(this::featureList.name)
+
     override fun permissionDefine(): PermissionDefineFunction = client.function(this::permissionDefine.name)
     override fun permissionGet(): PermissionGetFunction = client.function(this::permissionGet.name)
     override fun permissionList(): PermissionListFunction = client.function(this::permissionList.name)
