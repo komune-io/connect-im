7cc77733a60b617c7ac4e75d3a85270654bca87f
Adrien Poupard <adrien@komune.io>
2026-03-23 18:28:51 +0100

    Update fixers version to 0.29.0

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 dependencies {
-    implementation("io.komune.fixers.gradle:dependencies:0.28.0-SNAPSHOT")
+    implementation("io.komune.fixers.gradle:dependencies:0.29.0")
 }
 
 repositories {
